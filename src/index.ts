bfd9c5c651058ec55c916e931958a48fb864d9a1
cnyballk <584518260@qq.com>
2023-04-02 23:06:48 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+export * from 'ahooks';
+export { default as classnames } from 'classnames';
 export { default as CountUp } from './CountUp';
 export { default as Flipper } from './Flipper';
 export { default as Grid } from './Grid';
 export { default as List } from './List';
-export { default as Table } from './Table';
 export { default as Menu } from './Menu';
+export { default as Table } from './Table';
 export * from './utils';
