fd9e45e9d914d96fae9255bb7fa578882a38a623
cnyballk <584518260@qq.com>
2024-02-19 11:20:50 +0800

    feat: update

--- src/CountUp/index.tsx
+++ src/CountUp/index.tsx
@@ -1,9 +1,24 @@
-import React, { type FC } from 'react';
+import React, { useImperativeHandle, useState, type FC } from 'react';
 import ReactCountUp from 'react-countup';
 import { CountUpProps } from 'react-countup/build/CountUp';
 
-const CountUp: FC<CountUpProps> = (props) => (
-  <ReactCountUp duration={2.75} {...props}></ReactCountUp>
-);
+const CountUp: FC<CountUpProps> = (props, ref) => {
+  const { end, ...otherProps } = props;
+  const [currentEnd, setEnd] = useState(end);
+  useImperativeHandle(ref, () => {
+    return {
+      setEnd: function (end: number) {
+        setEnd(end);
+      },
+    };
+  });
+  return (
+    <ReactCountUp
+      duration={2.75}
+      end={currentEnd}
+      {...otherProps}
+    ></ReactCountUp>
+  );
+};
 
 export default CountUp;
